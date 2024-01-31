eaf9f302de390956257f40576e712133740c804a
Phillip Nyarko <phillip@PhillipsPC>
2024-01-30 22:25:03 -0600

    prottype done

--- capillary_wave.py
+++ capillary_wave.py
@@ -18,7 +18,7 @@ class CapillaryWave:
             new_ripple = Ripple(self.mouse_position, 0, self.color)
             self.ripples_list.append(new_ripple)
 
-        number_of_frames = 10
+        number_of_frames = 100
         for i in range(1, number_of_frames + 1):
 
             self.ripples_list[-1].radius += i ** 2 # increase the radius of the last ripple in the ripple list
@@ -35,10 +35,6 @@ class CapillaryWave:
 
             # pause temporarily and clear the window  
 
-            if i % 2 == 0:
-                self.window.fill(BACKGROUND_COLOR)
-                time.sleep(1)
-
-                
-
+            self.window.fill(BACKGROUND_COLOR)
+            time.sleep(0.05)
 
